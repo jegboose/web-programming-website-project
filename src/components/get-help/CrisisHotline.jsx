fbe719a2cc37cad5970fd6ecaec4f544af3e8c3c
bngeth <brandonngeth11@gmail.com>
2026-04-21 17:00:52 -0500

    Worked on a componet

---
+++ src/components/get-help/CrisisHotline.jsx
@@ -0,0 +1,15 @@
+export default function CrisisHotline() {
+    return (
+        <section>
+        <h1>Get Help / Resources</h1>
+        <h2>Crisis Hotlines</h2>
+        <p>
+          If you or a loved one are experiencing immediate distress from cyberbullying,
+          call or text <strong>988</strong> (Suicide &amp; Crisis Lifeline) in the US/Canada.
+          For youth, text <strong>"HOME"</strong> to <strong>741741</strong> to connect with
+          the <a href="https://www.crisistextline.org">Crisis Text Line</a>, or contact the <a href="https://www.stompoutbullying.org/helpchat">Stomp Out Bullying HelpChat Line</a>. These
+          services are free, confidential, and available 24/7.
+        </p>
+      </section>
+    )
+}
