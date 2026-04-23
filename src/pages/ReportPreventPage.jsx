f441704c1171a8a5002bfac7fd7eb30baa22f375
edislagu <edwinfc826@gmail.com>
2026-04-23 14:36:02 -0500

    added meta tags for page

--- src/pages/ReportPreventPage.jsx
+++ src/pages/ReportPreventPage.jsx
@@ -6,12 +6,23 @@ import Sidebar from "../components/ReportPrevent/Sidebar"
 import StepsList from '../components/ReportPrevent/StepsList'
 import "../components/ReportPrevent/Sidebar.css"
 
+import { Helmet } from 'react-helmet'
 
 export default function ReportPreventPage() {
 
   const[selected, setSelected] = useState(null)
 
   return (
+
+    <>
+      <Helmet>
+        <title> How to Report and Prevent Cyberbullying | Prevent Cyberbullying</title>
+        <meta name="description" content="Learn how to report cyberbullying on Social Media Platforms."/>
+        <meta name="keywords" content="cyberbullying, report, prevent, instagram, snapchat"/>
+        <meta name="authro" content="Edwin Islas Aguirre"/>
+      </Helmet>
+
+
     <div style={{display : 'flex'}}>
       <Sidebar onSelect={setSelected} />
     <main className="report-content">
@@ -19,5 +30,6 @@ export default function ReportPreventPage() {
       {selected === "prevent" ? <p>Prevention tips coming soon</p> : <StepsList platform={selected}/>}
     </main>
     </div>
+  </>
   )
 }
