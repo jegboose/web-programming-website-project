5a126fe6b019546278acce549e4b7fb75102d2ef
edislagu <edwinfc826@gmail.com>
2026-04-23 22:31:45 -0500

    added discord report images to page

--- src/components/ReportPrevent/StepsList.jsx
+++ src/components/ReportPrevent/StepsList.jsx
@@ -1,12 +1,19 @@
 // Data for steps list for how to report on social media platform 
 
+// Discord Report images
+import discordStep1 from '../../assets/rpp-images/discord-steps/discord-report1.jpeg'
+import discordStep2 from '../../assets/rpp-images/discord-steps/discord-report2.jpeg'
+import discordStep3 from '../../assets/rpp-images/discord-steps/discord-report3.jpeg'
+import discordStep4 from '../../assets/rpp-images/discord-steps/discord-report4.png'
+import discordStep5 from '../../assets/rpp-images/discord-steps/discord-report5.jpeg'
+
 const stepsData = { 
     Instagram: {
         icon: "📸",
         video: "https://www.youtube.com/embed/7ZdDVGpSJT0",
         steps: [
-        {text: "First, go to the account you want to report.", /* image */},
-        {text: "Tap the three dots in top right corner.", /* image */},
+        {text: "First, go to the account you want to report.", },
+        {text: "Tap the three dots in top right corner."},
         {text: "There is a tab with a red report button, press that"},
         {text: "There will be a tab that follows up with 'What do you want to report'"},
         {text: "Press any of the options that fit the description"}
@@ -29,11 +36,12 @@ const stepsData = {
         icon: "📸",
         video: "https://www.youtube.com/embed/zPwpHt5P38c?si=ZHNCJX8LXvKOIkWZ",
         steps: [
-        {text: "First, go to the account you want to report.", /* image */},
-        {text: "Tap the three dots in top right corner.", /* image */},
-        {text: "There is a tab with a red report button, press that"},
-        {text: "There will be a tab that follows up with 'What do you want to report'"},
-        {text: "Press any of the options that fit the description"}
+        {text: "Select the user you want to report.", image: discordStep1},
+        {text: "Press the user and the 3 dots (...) will appear on the top right corner of the user profile.", image: discordStep2},
+        {text: "It will then redirect you to a different page where you will report the user, after reading the text, press 'Next'", image: discordStep3},
+        {text: "The following screen will take you to another page, for your reasons to report someone.", image: discordStep4},
+        {text: "Press any of the applicable options and press 'Next' again."},
+        {text: "A report summary will then appear, outlining the reasons to the report. You can then proceed to report them, finalizing the report.", image: discordStep5},
         ]
     },
 
@@ -61,6 +69,7 @@ function StepsList({platform}) {
                 {steps.map((step, index) => ( 
                     <div key={index}>
                     <p>{step.text}</p>
+                    {step.image && <img src={step.image} alt={`Step ${index + 1}`} className="step-image"/>}
                     </div>
                 ))}
 
