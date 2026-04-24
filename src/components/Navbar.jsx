946a6322ba014037756799556601afbd38ce8fa2
Ahmed Al-Kaisi <ahmedalkaisi04@gmail.com>
2026-04-24 15:41:56 -0500

    added some icons to the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import logo from '../assets/perventcyberbullying-logo.png'
+import { RxAccessibility } from "react-icons/rx";
+import { FaHome } from "react-icons/fa";
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 
@@ -9,11 +11,12 @@ function Navbar() {
             <nav className="navbar">
                 <Link to="/"><img src={logo} alt="Pervent Cyberbullying Logo" className="navbar-logo" /></Link>
                 <ul className="navbar-links">
+                    <li><Link to="/" aria-label="Home"><FaHome /></Link></li>
                     <li><Link to="/what-is-cyberbullying">What Is It?</Link></li>
                     <li><Link to="/get-help">Get Help</Link></li>
                     <li><Link to="/report-prevent">Report & Prevent</Link></li>
                     <li><Link to="/stories">Stories</Link></li>
-                    <li><Link to="/accessibility">Accessibility</Link></li>
+                    <li><Link to="/accessibility" aria-label="Accessibility"><RxAccessibility /></Link></li>
 
                 </ul>
             </nav>
